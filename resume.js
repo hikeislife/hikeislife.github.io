da5a62f9d1bd876990e09a000b24b6c2e0d42867
hikeislife <hikeislife@users.noreply.github.com>
2019-07-17 21:49:50 +0200

    resume

--- resume.js
+++ resume.js
@@ -14,18 +14,21 @@ hide = (item) => {
 navigate = (nav) => {
 	nav.addEventListener(`click`, (e) => {
 		$(".selection").style.opacity = 0;
+		$(`.${e.target.id}`).style.opacity = 0;
+		console.log($(`.${e.target.id}`))
 		navs.forEach(reset);
 		nav.classList.add('active')
 		// reposition title to make room for additional content
 		$('h1').style.fontSize = `2rem`;
-		$('h1').style.transform = "translate(0, 90px)";
+		$('h1').style.transform = "translate(0, -20px)";
 
 		document.querySelectorAll('.loadThis').forEach(hide);
 
 		setTimeout(() => {
 		 	$(".selection").innerHTML = `<br /> ${nav.innerHTML}`;
-		 	$(".selection").style.opacity = 1;
 		 	$(`.${e.target.id}`).style.display = "inline";
+		 	$(".selection").style.opacity = 1;
+		 	$(`.${e.target.id}`).style.opacity = 1;
 		}, 600);
 		
 		
